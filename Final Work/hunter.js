3d6d223b795bb8b1f6f552c0e3741bb4777bbd1b
YurZTumo <yuri.zakaryan@tumo.org>
2022-10-11 16:54:24 +0400

    tumo2

--- Final Work/hunter.js
+++ Final Work/hunter.js
@@ -1,4 +1,6 @@
-class Hunter extends LivingCreature{
+let LivingCreature = require('./LivingCreature')
+
+module.exports = class Hunter extends LivingCreature{
     constructor(x, y) {
         super(x,y)
         this.energy = 10;
